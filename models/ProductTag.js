79cccc4bfbbd45687265ee5765a33e6a43e36d5e
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-21 22:46:25 -0600

    updating models folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,25 +6,24 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Product",
-        key: "Id",
+        model: "product",
+        key: "id",
       },
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Tag",
-        key: "Id",
+        model: "tag",
+        key: "id",
       },
     },
   },
@@ -33,7 +32,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
